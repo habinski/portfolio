f56f13d7fa4bb15158ca8dd0ee9cfd45e2d0e569
habinski <matthewschmtz@gmail.com>
2020-09-17 21:05:54 +0200

    little update

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,25 +1,4 @@
 import { Link } from "gatsby"
-// import React from "react"
-// import logo from "../assets/logo.svg"
-// // import { FaAlignRight } from "react-icons/fa"
-// import PageLinks from "../constants/links"
-// const Navbar = ({ toggleSidebar }) => {
-// 	return (
-// 		<nav className="navbar">
-// 			<img src={logo} alt="logo" />
-
-// 			{/* <button type="button" className="toggle-btn" onClick={toggleSidebar}>
-// 					<FaAlignRight></FaAlignRight>
-// 				</button> */}
-// 			<div className="line-nav">
-// 				<PageLinks />
-// 			</div>
-// 		</nav>
-// 	)
-// }
-
-// export default Navbar
-
 
 import React from "react"
 import logo from "../assets/logo.svg"
