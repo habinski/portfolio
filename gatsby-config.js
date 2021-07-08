e92386a914468de80bb96ebe660d24aa8d234559
habinski <matthewschmtz@gmail.com>
2021-07-09 00:31:06 +0200

    return to localhost api

--- gatsby-config.js
+++ gatsby-config.js
@@ -26,7 +26,7 @@ module.exports = {
         {
             resolve: "gatsby-source-strapi",
             options: {
-                apiURL: "http://localhost:1337/",
+                apiURL: "http://localhost:1337",
                 contentTypes: [
                     "article",
                     "category",
