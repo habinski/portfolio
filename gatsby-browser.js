0e1a520dcfb0305dc480f0be3ecf982237ebc0ef
habinski <matthewschmtz@gmail.com>
2021-09-07 00:10:46 +0200

    Added page transition

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,5 +1,29 @@
 import "./src/css/main.scss";
-// import "./prismjs/themes/prism-tomorrow.css";
-// import "./prismjs/plugins/line-numbers/prism-line-numbers.css";
-// require("prismjs/plugins/line-numbers/prism-line-numbers.css");
-// require("prismjs/themes/prism-solarizedlight.css")
+
+import React from 'react';
+import { AnimatePresence } from 'framer-motion';
+
+
+export const wrapPageElement = ({ element }) => (
+	<AnimatePresence exitBeforeEnter>{element}</AnimatePresence>
+);
+
+
+// export const shouldUpdateScroll = ({
+// 	routerProps: { location },
+// 	getSavedScrollPosition
+// }) => {
+// 	// transition duration from `layout.js` * 1000 to get time in ms
+// 	// * 2 for exit + enter animation
+// 	const TRANSITION_DELAY = 0.3 * 1000 * 2
+// 	// if it's a "normal" route
+// 	if (location.action === "PUSH") {
+// 		window.setTimeout(() => window.scrollTo(0, 0), TRANSITION_DELAY)
+// 	}
+// 	// if we used the browser's forwards or back button
+// 	else {
+// 		const savedPosition = getSavedScrollPosition(location) || [0, 0]
+// 		window.setTimeout(() => window.scrollTo(...savedPosition), TRANSITION_DELAY)
+// 	}
+// 	return false
+// }
--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,9 +1,11 @@
 import React from "react"
-// import "../css/main.css"
 import Navbar from "./Navbar"
 import Sidebar from "./Sidebar"
 import CookiesBanner from "./CookiesBanner"
 import Footer from "./Footer"
+
+import { motion } from "framer-motion"
+
 const Layout = ({ children, title }) => {
 	const [isOpen, setIsOpen] = React.useState(false)
 	const toggleSidebar = () => {
@@ -14,9 +16,19 @@ const Layout = ({ children, title }) => {
 		<>
 			<Navbar toggleSidebar={toggleSidebar} title={title} />
 			<Sidebar isOpen={isOpen} toggleSidebar={toggleSidebar} />
-			<main>{children}
+			<motion.main
+				initial={{ opacity: 0, x: -200 }}
+				animate={{ opacity: 1, x: 0 }}
+				exit={{ opacity: 0, x: 200 }}
+				transition={{
+					type: "spring",
+					mass: 0.35,
+					stiffness: 75,
+					duration: 0.3
+				}}
+			>{children}
 				<CookiesBanner />
-			</main>
+			</motion.main>
 			<Footer />
 		</>
 	)
